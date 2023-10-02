543e990c514289aa2034689d1b8afa9b931015b5
rxthew <richardtheumaster@gmail.com>
2023-10-02 02:26:58 +0200

    Enable connection of players
    Set up events & placeholder canvas.

--- public/client.js
+++ public/client.js
@@ -1,1 +1,55 @@
 const socket = io();
+
+
+const canvas = document.getElementById('game-canvas');
+const ctx = canvas.getContext('2d');
+
+let playerId; // Store the player's ID
+
+//Define canvas dimensions
+const viewportWidth = 0.75 * window.innerWidth;
+const viewportHeight = 0.75 * window.innerHeight;
+canvas.width = viewportWidth;
+canvas.height = viewportHeight;
+
+
+// Initialize the canvas with the received viewport dimensions
+socket.on('initPlayer', (data) => {
+  playerId = data.id;
+
+  drawPlayer(data);
+});
+
+socket.on('newPlayer', (data) => {
+  
+  drawPlayer(data);
+});
+
+socket.on('playerDisconnected', (playerId) => {
+  delete players[playerId];
+});
+
+function drawPlayer(player) {
+  // Set the visible area dimensions based on the world dimensions
+  const visibleWidth = player.worldWidth/10;
+  const visibleHeight = player.worldHeight/10;
+
+  
+  //Calculate the scale factor for the visible area based on the client's viewport dimensions
+  const scaledWidthFactor = visibleWidth/viewportWidth;
+  const scaledHeightFactor = visibleHeight/viewportHeight;
+  
+
+  // Calculate the circle's radius as a fraction of the viewport's width
+
+  ctx.scale(scaledWidth,scaledHeight);
+  ctx.beginPath();
+  ctx.arc(100,75, 50, 0, Math.PI * 2);
+  ctx.fillStyle = 'blue'; // You can assign different colors to different players
+  ctx.fill();
+  ctx.closePath();
+}
+
+
+
+// ... (handle player input and other game logic)
--- server.js
+++ server.js
@@ -2,28 +2,77 @@
 
 const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io');
+const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = new Server(server);
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/',(req,res)=>{
+  res.sendFile(__dirname + '/index.html')
+})
+
+
+const players = {}; // Store player positions
+
+const worldWidth = 20000
+const worldHeight = 6000
+
 io.on('connection', (socket) => {
   console.log('A user connected');
+
+  // Generate a random position for the player within the game world
+  const playerX = Math.random() * worldWidth;
+  const playerY = Math.random() * worldHeight;
+
+  // Create a unique player ID based on the socket ID
+  const playerId = socket.id;
+
+  // Store the player's position
+  players[playerId] = { x: playerX, y: playerY };
+
+  // Send the player's ID, position, and world dimensions to the client
+  socket.emit('initPlayer', {
+    id: playerId,
+    x: playerX,
+    y: playerY,
+    worldWidth,
+    worldHeight
+  });
+
+  // Broadcast the new player to other clients
+  socket.broadcast.emit('newPlayer',
+  {
+    id: playerId,
+    x: playerX,
+    y: playerY,
+    worldWidth,
+    worldHeight
+  }
+   );
 
   // Handle disconnection
   socket.on('disconnect', () => {
     console.log('A user disconnected');
+
+    // Remove the player from the server-side player list
+    delete players[playerId];
+
+    // Broadcast to other clients that this player has disconnected
+    socket.broadcast.emit('playerDisconnected', playerId);
   });
 
-  // Handle custom events here
-  // Example: socket.on('chat message', (msg) => { io.emit('chat message', msg); });
+  
 });
+
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
+
